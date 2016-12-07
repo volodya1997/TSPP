5ff53f9ecb04d69a152408bb21c1ed11f1e90c46
volodya1997 <mega-volodya.glivka@ukr.net>
2016-12-07 21:24:03 +0200

    Name

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -9,7 +9,8 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello word");
+            Console.WriteLine("Введіть імя");
+            Console.ReadLine();
         }
     }
 }
